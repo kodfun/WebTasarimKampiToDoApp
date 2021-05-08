78527ef518162aa3fb993a5958a9e6863f524706
KodluYorum <info@kodluyorum.dev>
2021-05-08 19:02:56 +0300

    store/load data

--- js/site.js
+++ js/site.js
@@ -1,41 +1,111 @@
-let todos = [
-    {
-        id: 1,
-        task: "Do homework",
-        isDone: false
-    },
-    {
-        id: 2,
-        task: "Clean your room",
-        isDone: true
-    },
-    {
-        id: 3,
-        task: "Sleep",
-        isDone: false
-    },
-];
+let todos = [];
+// let todos = [
+//     {
+//         id: 1,
+//         task: "Do homework",
+//         isDone: false
+//     },
+//     {
+//         id: 2,
+//         task: "Clean your room",
+//         isDone: true
+//     },
+//     {
+//         id: 3,
+//         task: "Sleep",
+//         isDone: false
+//     },
+// ];
 
 // EVENTS
+// NEW TODO
 $("#frmTodo").submit(function(event) {
     event.preventDefault();
     let todo = {
-        id: 0,
+        id: maxId() + 1,
         task: $("#inputTask").val().trim(),
         isDone: false
     }
     todos.push(todo);
+    saveData();
     this.reset();
     listTodos();
 });
 
+// DONE/UNDONE
+$("body").on("change", "input[type='checkbox'][data-id]", function() {
+    let id = $(this).data("id");
+    let isDone = $(this).prop("checked");
+    let todo = getTodoById(id);
+    todo.isDone = isDone;
+    saveData();
+    listTodos();
+});
+
+// DELETE
+$("body").on("dblclick", "li[data-id]", function() {
+    if (!confirm("Are you sure to delete the todo item?"))
+        return;
+    let id = $(this).data("id");
+    deleteById(id);
+    saveData();
+    listTodos();
+});
+
+
 // FUNCTIONS
+function saveData() {
+    localStorage["data"] = JSON.stringify(todos);
+}
+
+function loadData() {
+    try {
+        todos = JSON.parse(localStorage["data"]);
+    }
+    catch {
+        todos = [];
+    }
+}
+
+function deleteById(id) {
+    let index;
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            index = i;
+            break;
+        }
+    }
+    todos.splice(index, 1);
+}
+
+function getTodoById(id) {
+    for (const todo of todos) {
+        if (todo.id == id)
+            return todo;
+    }
+    return null;
+}
+
+function maxId() {
+    let max = 0;
+
+    for (const todo of todos) {
+        if (todo.id > max)
+            max = todo.id;
+    }
+
+    return max;
+}
+
 function listTodos() {
     $("ul#todos").html("");
-    $.each(todos, function(index, todo) {
-        let li = $("<li/>");
+    let sortedTodos = todos.sort((a,b) => a.isDone - b.isDone);
+    $.each(sortedTodos, function(index, todo) {
+        let li = $("<li/>")
+            .attr("data-id", todo.id);
         let cb = $("<input/>")
-                    .attr("type", "checkbox");
+                    .attr("type", "checkbox")
+                    .attr("data-id", todo.id);
         if (todo.isDone) {
             cb.prop("checked", true);
             li.addClass("done");
@@ -49,4 +119,5 @@ function listTodos() {
     });
 }
 
+loadData();
 listTodos();
